58e5414479a0c8e10f64264f2acbba60b79745cf
Dan <danjthorne@hotmail.co.uk>
2015-07-03 22:00:30 +0100

    DatabaseFinetuning

--- database.sql
+++ database.sql
@@ -1,53 +1,65 @@
 CREATE DATABASE database;
 USE database;
 
-DROP TABLE IF EXISTS `Order`;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS product;
-DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `customer`;
+DROP TABLE IF EXISTS `product`;
+DROP TABLE IF EXISTS `category`;
 
-CREATE TABLE IF NOT EXISTS category(
-	id       int  primary key,
-    name     varchar(20) NOT NULL,
-    key( name)
-)ENGINE=InnoDB CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS category (
+  `id`   INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(20) NOT NULL,
+  KEY (name)
+)
+  ENGINE = InnoDB
+  CHARSET = latin1;
 
 CREATE TABLE IF NOT EXISTS product (
-    product_id          int                 primary key auto_increment,
-    name        varchar(100)         NOT NULL,
-    price       decimal(8,2)        NOT NULL,
-    description text,
-    stock       int unsigned  NOT NULL default 0,
-    category_id    int          NOT NULL,
-	CONSTRAINT `category` FOREIGN KEY (category_id) REFERENCES category (id)
-) ENGINE=InnoDB CHARSET=latin1;
+  `product_id`  INT PRIMARY KEY        AUTO_INCREMENT,
+  `name`        VARCHAR(100)  NOT NULL,
+  `price`       DECIMAL(8, 2) NOT NULL,
+  `description` TEXT,
+  `stock`       INT UNSIGNED  NOT NULL DEFAULT 0,
+  `category_id` INT           NOT NULL,
+  CONSTRAINT `category` FOREIGN KEY (category_id) REFERENCES category (id)
+)
+  ENGINE = InnoDB
+  CHARSET = latin1;
 
 
-CREATE TABLE IF NOT EXISTS customer (
-    id      int          primary key auto_increment,
-    name    varchar(50)  NOT NULL,
-    addressLine1 varchar(30) NOT NULL,
-	addressLine2 varchar(30),
-	addressLine3 varchar(30),
-	addressLine4 varchar(30),
-	
-    unique key (name, address)
-) ENGINE=InnoDB CHARSET=latin1;
-	
+CREATE TABLE IF NOT EXISTS `customer` (
+  `id`              INT PRIMARY KEY AUTO_INCREMENT,
+  `name`             VARCHAR(50)  NOT NULL,
+  `address_house`    VARCHAR(255) NOT NULL,
+  `address_street`   VARCHAR(255) NOT NULL,
+  `address_town`     VARCHAR(255) NOT NULL,
+  `address_county`   VARCHAR(255) NOT NULL,
+  `address_postcode` VARCHAR(255) NOT NULL,
+  `address_country`  VARCHAR(255) NOT NULL,
+
+  UNIQUE KEY (`name`, `address`)
+)
+  ENGINE = InnoDB
+  CHARSET = latin1;
+
 CREATE TABLE IF NOT EXISTS `order` (
-    customer_id         int           primary key auto_increment,
-    customer   int           NOT NULL,
-)ENGINE=InnoDB CHARSET=latin1;
+  `customer_id`  INT PRIMARY KEY AUTO_INCREMENT,
+  `customer` INT NOT NULL
+)
+  ENGINE = InnoDB
+  CHARSET = latin1;
 
 CREATE TABLE IF NOT EXISTS order_product (
-  order_id int(10) unsigned NOT NULL,
-  product_id int(10) unsigned NOT NULL,
-  price double NOT NULL,
-  PRIMARY KEY (id),
-  KEY product (product_id),
-  CONSTRAINT product FOREIGN KEY (product_id) REFERENCES product (id)
-  CONSTRAINT `order` FOREIGN KEY (order_id) REFERENCES order (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `order_id`   INT UNSIGNED NOT NULL,
+  `product_id` INT UNSIGNED NOT NULL,
+  `price`      DOUBLE           NOT NULL,
+  KEY `product` (order_id),
+  KEY `product` (product_id),
+  CONSTRAINT `product` FOREIGN KEY (product_id) REFERENCES `product` (id),
+    CONSTRAINT `order` FOREIGN KEY (order_id) REFERENCES `order` (id)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
 
 
@@ -65,6 +77,5 @@ CREATE TABLE IF NOT EXISTS order_product (
 
 
 
-	
 	
 	
